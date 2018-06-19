1564c1fbe13dfe572b03023689d4d9dc14177214
Kim Pham <kimpham@Kims-MacBook-Pro.local>
2018-06-19 20:00:44 +0700

    Add array file. Add gitignore

---
+++ array.js
@@ -0,0 +1,10 @@
+var names = [];
+//way to use function of object
+//push(item) add item to array
+names.push('Kim');
+names.push('Thach');
+
+//forEach loop array
+names.forEach(item => {
+    console.log(item);
+});
